ca35bc4562dccc76b054266bd34ece844e9b7750
Jeff Miller <jmiller@ncsasports.org>
2019-10-21 12:15:23 -0500

    Add Font Awesome, GA and display locks & win line.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,9 @@ import { css } from "../styles/nba-picks.css"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
+
 
 export default ({ data }) => {
   return (
@@ -23,10 +26,12 @@ export default ({ data }) => {
       <tbody>
       {data.allPicksJson.edges.map(({ node }) => (
         <tr key={node.id}>
-          <td><img src={node.team_src} /></td>
-          <td className={node.bill}>{node.bill}</td>
-          <td className={node.ryen}>{node.ryen}</td>
-          <td className={node.house}>{node.house}</td>
+          <td><img src={node.team_src} /><br />{node.line}</td>
+          <td className={node.bill}>
+            {node.bill}{node.bill_lock && (<React.Fragment><br /><FontAwesomeIcon icon={faLock} /></React.Fragment>)}
+          </td>
+          <td className={node.ryen}>{node.ryen}{node.ryen_lock && (<React.Fragment><br /><FontAwesomeIcon icon={faLock} /></React.Fragment>)}</td>
+          <td className={node.house}>{node.house}{node.house_lock && (<React.Fragment><br /><FontAwesomeIcon icon={faLock} /></React.Fragment>)}</td>
         </tr>
       ))}
       </tbody>
@@ -42,9 +47,13 @@ query {
       node {
         id
         bill
+        bill_lock
         house
+        house_lock
         ryen
+        ryen_lock
         team_src
+        line
       }
     }
   }
